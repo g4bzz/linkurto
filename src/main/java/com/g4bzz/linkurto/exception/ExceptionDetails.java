05597d8cb554adb209fe8113c4e53faa8ad4c857
g4bzz <gabriel.god360@gmail.com>
2025-05-17 13:26:37 -0300

    refactor: remove unused imports

--- src/main/java/com/g4bzz/linkurto/exception/ExceptionDetails.java
+++ src/main/java/com/g4bzz/linkurto/exception/ExceptionDetails.java
@@ -1,6 +1,5 @@
 package com.g4bzz.linkurto.exception;
 
-import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 
--- src/main/java/com/g4bzz/linkurto/exception/ValidationExceptionDetails.java
+++ src/main/java/com/g4bzz/linkurto/exception/ValidationExceptionDetails.java
@@ -1,11 +1,9 @@
 package com.g4bzz.linkurto.exception;
 
-import lombok.Data;
 import lombok.experimental.SuperBuilder;
 
 import java.util.Map;
 
-@Data
 @SuperBuilder
 public class ValidationExceptionDetails extends ExceptionDetails {
     Map<String,String> fields;

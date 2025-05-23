c76a6188a6a12a5a42389ded37a68a2df35b4104
g4bzz <gabriel.god360@gmail.com>
2025-05-23 00:05:07 -0300

    refactor: add @builder annotation to DTOs

--- src/main/java/com/g4bzz/linkurto/dto/UrlPostRequestBody.java
+++ src/main/java/com/g4bzz/linkurto/dto/UrlPostRequestBody.java
@@ -3,10 +3,12 @@ package com.g4bzz.linkurto.dto;
 import com.g4bzz.linkurto.constraints.ValidUrl;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class UrlPostRequestBody {
--- src/main/java/com/g4bzz/linkurto/dto/UrlPostResponseBody.java
+++ src/main/java/com/g4bzz/linkurto/dto/UrlPostResponseBody.java
@@ -1,11 +1,13 @@
 package com.g4bzz.linkurto.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class UrlPostResponseBody {

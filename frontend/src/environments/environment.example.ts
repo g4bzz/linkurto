5df9f4d32cf4346046747ef05c232935db56b36a
g4bzz <gabriel.god360@gmail.com>
2025-06-28 00:38:12 -0300

    doc: adds example environment file for frontend project

---
+++ frontend/src/environments/environment.example.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  github_url: 'GITHUB_URL',
+  linkedin_url: 'LINKEDIN_URL',
+  recaptcha_sitekey: 'RECAPTCHA_SITEKEY',
+  linkurto_server_url: 'http://localhost:8080/',
+  linkurto_proxy_server_url: 'http://localhost:4200/api/',
+};

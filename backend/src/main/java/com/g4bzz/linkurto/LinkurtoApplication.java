1df5fe73894b7718fa146c38cba209abed291775
g4bzz <gabriel.god360@gmail.com>
2025-07-17 21:25:03 -0300

    feat: adds grafana

--- backend/src/main/java/com/g4bzz/linkurto/LinkurtoApplication.java
+++ backend/src/main/java/com/g4bzz/linkurto/LinkurtoApplication.java
@@ -1,7 +1,11 @@
 package com.g4bzz.linkurto;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -9,5 +13,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class LinkurtoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LinkurtoApplication.class, args);
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
 	}
 }

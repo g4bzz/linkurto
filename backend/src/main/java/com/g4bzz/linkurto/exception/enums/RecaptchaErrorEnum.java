b30e2b7fc86fb5d1622bb382dcb253c61832e742
g4bzz <gabriel.god360@gmail.com>
2025-12-22 23:21:10 -0300

    feat: add new enum const to incorrect captcha solution exception from recaptcha

--- backend/src/main/java/com/g4bzz/linkurto/exception/enums/RecaptchaErrorEnum.java
+++ backend/src/main/java/com/g4bzz/linkurto/exception/enums/RecaptchaErrorEnum.java
@@ -8,12 +8,13 @@ public enum RecaptchaErrorEnum {
     MISSING_INPUT_RESPONSE("O token do captcha está ausente."),
     INVALID_INPUT_RESPONSE("O token do captcha é inválido ou está incorreto."),
     BAD_REQUEST("Houve um problema na verificação do captcha."),
-    TIMEOUT_OR_DUPLICATE("O token do captcha não é mais válido: ou é muito antigo ou já foi usado");
+    TIMEOUT_OR_DUPLICATE("O token do captcha não é mais válido: ou é muito antigo ou já foi usado"),
+    INCORRECT_CAPTCHA_SOL("A resposta do captcha está incorreta.");
 
     @Getter
     private final String message;
+
     RecaptchaErrorEnum(String message) {
         this.message = message;
     }
-
 }
